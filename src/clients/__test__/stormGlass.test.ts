b5c7e71dc767775c4e2a01c3790f108a70fe8833
Drinnn <pedro_lontro@hotmail.com>
2022-08-22 10:40:33 -0300

    test(wip): add normalized forecast first test

---
+++ src/clients/__test__/stormGlass.test.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import { StormGlass } from '@src/clients/stormGlass';
+import stormGlassWeather3HoursFixture from '@test/fixtures/stormglass_weather_3_hours.json';
+import stormGlassWeatherNormalized3HoursFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
+
+jest.mock('axios');
+
+describe('StormGlass client', () => {
+  it('should return the normalized forecast from the StormGlass service', async () => {
+    const lat = -150.123456;
+    const lng = 12.654321;
+
+    axios.get = jest.fn().mockResolvedValue(stormGlassWeather3HoursFixture);
+
+    const stormGlass = new StormGlass(axios);
+    const response = await stormGlass.fetchPoints(lat, lng);
+
+    expect(response).toEqual(stormGlassWeatherNormalized3HoursFixture);
+  });
+});
